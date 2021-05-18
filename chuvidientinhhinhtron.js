c3cdaa7ac0a01a374a4e0a6fa93014a674982ac4
quynhluyenthi <quynhluyenthi@gmail.com>
2021-05-18 20:21:48 +0700

    tính chu vi diện tích hình tròn

---
+++ chuvidientinhhinhtron.js
@@ -0,0 +1,6 @@
+let r = +prompt ('Nhập bán kính hình tròn:');
+const pi=3.14;
+C = 2*r*pi;
+S = (r*r)*pi;
+document.write ('Chu vi hình tròn là:' +C + '<br>');
+document.write ('Diện tích hình tròn:' +S);
